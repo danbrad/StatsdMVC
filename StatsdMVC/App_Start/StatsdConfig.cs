6033bb34497da93db82fd08917b037777de7783f
Dan Bradley <dan@danbrad.com>
2015-03-07 10:25:27 +0000

    Rename to StatsdMVC

---
+++ StatsdMVC/App_Start/StatsdConfig.cs
@@ -0,0 +1,23 @@
+﻿using StatsdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace StatsdMVC
+{
+    public static class StatsdConfig
+    {
+        public static void Register()
+        {
+            var metricsConfig = new MetricsConfig
+            {
+                StatsdServerName = "localhost",
+                Prefix = "StatsdMVC",
+                StatsdServerPort = 8125
+            };
+
+            StatsdClient.Metrics.Configure(metricsConfig);
+        }
+    }
+}
---
+++ StatsdMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using StatsdMVC.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StatsdMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [StatsdTimer]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
