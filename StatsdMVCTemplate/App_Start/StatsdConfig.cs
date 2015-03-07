e2e6936e0718bb5fafe9dcb64fdfbd1335eb1b15
Dan Bradley <dan@danbrad.com>
2015-03-07 10:19:41 +0000

    Add config for statsd and an attribute to specify action should be timed.

---
+++ StatsdMVCTemplate/App_Start/StatsdConfig.cs
@@ -0,0 +1,23 @@
+﻿using StatsdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace StatsdMVCTemplate
+{
+    public static class StatsdConfig
+    {
+        public static void Register()
+        {
+            var metricsConfig = new MetricsConfig
+            {
+                StatsdServerName = "localhost",
+                Prefix = "StatsdMVCTemplate",
+                StatsdServerPort = 8125
+            };
+
+            StatsdClient.Metrics.Configure(metricsConfig);
+        }
+    }
+}
--- StatsdMVCTemplate/Controllers/HomeController.cs
+++ StatsdMVCTemplate/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StatsdMVCTemplate.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace StatsdMVCTemplate.Controllers
 {
     public class HomeController : Controller
     {
+        [StatsdTimer]
         public ActionResult Index()
         {
             return View();
